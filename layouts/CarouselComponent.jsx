89fdcb00d1d6a81ab41204caa658d04c9b60ff05
sbhole61299 <shubhambhole68@gmail.com>
2023-07-04 01:50:27 +0530

    final

--- layouts/CarouselComponent.jsx
+++ layouts/CarouselComponent.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const CarouselComponent = () => {
-
   const testimonials = [
     {
       id: 1,
       name: 'Kshitija Kubal',
-      testimonial: 'Good understanding of requirements, can learn any skills quickly and his guidance is amazing. Working with him was a great experience.',
+      testimonial:
+        'Good understanding of requirements, can learn any skills quickly and his guidance is amazing. Working with him was a great experience.',
     },
     {
       id: 2,
-      name: 'Jane Smith',
-      testimonial: 'Pellentesque ullamcorper augue vel risus semper, sed vestibulum quam cursus.',
+      name: 'Satwik ra',
+      testimonial: 'A great developer with deep and broad technical experience.',
     },
     {
       id: 3,
-      name: 'Bob Johnson',
-      testimonial: 'Donec ullamcorper risus a ipsum finibus, ut fermentum mauris dapibus.',
+      name: 'Amit Sharma',
+      testimonial: 'An excellent developer with fast comprehending new technology.',
     },
-  ];
+    {
+      id: 4,
+      name: 'SIDDHARTH GHOLAP',
+      testimonial: 'Awesome web developer',
+    },
+  ]
   return (
     <Carousel>
-    {testimonials.map((testimonial) => (
-      <div key={testimonial.id}>
-        <h3>{testimonial.name}</h3>
-        <p>{testimonial.testimonial}</p>
-      </div>
-    ))}
-  </Carousel>
-  );
-};
+      {testimonials.map((testimonial) => (
+        <div key={testimonial.id} style={{ padding: '20px', textAlign: 'center' }}>
+          <h3 style={{ marginBottom: '10px' }}>{testimonial.name}</h3>
+          <p style={{ fontSize: '16px' }}>{testimonial.testimonial}</p>
+        </div>
+      ))}
+    </Carousel>
+  )
+}
 
-export default CarouselComponent;
+export default CarouselComponent
