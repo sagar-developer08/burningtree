3fcd81ad8afb226e3a3bcdb42956da01de951e87
sagar <sagar@sagars-MacBook-Air.local>
2023-07-06 18:16:32 +0530

    changes

--- layouts/CarouselComponent.jsx
+++ layouts/CarouselComponent.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { AiFillStar } from 'react-icons/ai'
 
 const CarouselComponent = () => {
   const testimonials = [
     {
       id: 1,
-      name: 'Kshitija Kubal',
+      name: 'Sameer C.R',
       testimonial:
-        'Good understanding of requirements, can learn any skills quickly and his guidance is amazing. Working with him was a great experience.',
+        'Had a hand on experience in backend Fullfill our bussiness need and also Great ,perfect time of Delivery.',
     },
     {
       id: 2,
       name: 'Satwik ra',
-      testimonial: 'A great developer with deep and broad technical experience.',
+      testimonial: 'Has worked with for bulk delivery system has great knownlege of backend and frontEnd.Eager to learn new things and Full needs .',
     },
     {
       id: 3,
@@ -23,19 +24,104 @@ const CarouselComponent = () => {
     {
       id: 4,
       name: 'SIDDHARTH GHOLAP',
-      testimonial: 'Awesome web developer',
+      testimonial: 'Awesome web developer Agency',
     },
   ]
+  const carouselStyle = {
+    position: 'relative',
+    width: '100%',
+  }
+
+  const slideStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '300px', // Adjust the height according to your needs
+  }
+
+  const arrowStyle = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    background: 'transparent',
+    border: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+  }
+
+  const prevArrowStyle = {
+    ...arrowStyle,
+    left: '10px', // Adjust the positioning according to your needs
+  }
+
+  const nextArrowStyle = {
+    ...arrowStyle,
+    right: '10px', // Adjust the positioning according to your needs
+  }
+
+  const testimonialStyle = {
+    padding: '20px',
+    textAlign: 'center',
+  }
+
+  const nameStyle = {
+    marginBottom: '10px',
+  }
+
+  const textStyle = {
+    fontSize: '16px',
+  }
+  const starStyle = {
+    color: 'gold',
+    marginRight: '5px',
+  }
+
   return (
-    <Carousel>
+    <>
+    <h1 className="company-name">Our Reviews</h1>
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      swipeable={true}
+      emulateTouch={true}
+      renderArrowPrev={(onClickHandler, hasPrev, label) =>
+        hasPrev && (
+          <button
+            type="button"
+            style={prevArrowStyle}
+            onClick={onClickHandler}
+            title={label}
+          />
+        )
+      }
+      renderArrowNext={(onClickHandler, hasNext, label) =>
+        hasNext && (
+          <button
+            type="button"
+            style={nextArrowStyle}
+            onClick={onClickHandler}
+            title={label}
+          />
+        )
+      }
+    >
       {testimonials.map((testimonial) => (
-        <div key={testimonial.id} style={{ padding: '20px', textAlign: 'center' }}>
-          <h3 style={{ marginBottom: '10px' }}>{testimonial.name}</h3>
-          <p style={{ fontSize: '16px' }}>{testimonial.testimonial}</p>
+        <div
+          key={testimonial.id}
+          style={testimonialStyle}
+        >
+          <h3 style={nameStyle}>{testimonial.name}</h3>
+          <div style={textStyle}>
+            <AiFillStar style={starStyle} />
+            {testimonial.testimonial}
+          </div>
         </div>
       ))}
     </Carousel>
+
+    </>
   )
 }
+
 
 export default CarouselComponent
--- layouts/Projects.jsx
+++ layouts/Projects.jsx
@@ -10,7 +10,7 @@ const Layout = () => {
   return(
     <>
       <P1/>
-      <br/><br/>
+      
       <Carousel/>
     </>
   )
